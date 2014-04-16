73c51bda0341f9eb39025cb9041338d5ff287136
WGautier <williamg@activestate.com>
2014-04-16 16:32:56 +0200

    Add hello-java upload test

--- src/main/java/com/bcferrycoder/cflab/SimpleCFClient.java
+++ src/main/java/com/bcferrycoder/cflab/SimpleCFClient.java
@@ -13,25 +13,33 @@ import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.cloudfoundry.client.lib.domain.CloudApplication;
 import org.cloudfoundry.client.lib.domain.CloudService;
 import org.cloudfoundry.client.lib.domain.CloudSpace;
+import org.cloudfoundry.client.lib.domain.Staging;
 
 import javax.net.ssl.*;
+import java.io.File;
 import java.net.URI;
 import java.net.URL;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleCFClient {
     public static void main(String[] args) throws Exception {
 
 
-        String target = args[0];
-        if (target == null || target.equals("noop")) {
-	  System.exit(0);
+        String targetName = args[0];
+        if (targetName == null || targetName.equals("noop")) {
+            System.exit(0);
         }
+        String target = "https://api." + targetName;
         String user = args[1];
         String password = args[2];
+        String organization = args[3];
+        String cloudSpace = args[4];
+        String warPath = "hello-java-1.0.war";
 
         CloudCredentials credentials = new CloudCredentials(user, password);
-        CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL(target));
+        CloudFoundryClient client = new CloudFoundryClient(credentials, getTargetURL(target), organization, cloudSpace);
         client.login();
 
         System.out.println("\nTargeting Stackato instance " + target + " as user " + user);
@@ -46,11 +54,35 @@ public class SimpleCFClient {
             System.out.println(app.getName());
         }
 
-        System.out.println("\nServices");
+        System.out.println("\nServices:");
         for (CloudService service : client.getServices()) {
             System.out.println(service.getName() + ":" + service.getLabel());
         }
         System.out.println("\n");
+
+        String appName = "hello-java-cflab";
+        List<CloudApplication> existingApps = client.getApplications();
+        boolean found = false;
+        for (CloudApplication existingApp : existingApps) {
+            if (existingApp.getName().equals(appName)) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("\nApp " + appName + " already exists, skipping creation.");
+        } else {
+            System.out.println("\nCreating new app " + appName);
+            Staging staging = new Staging();
+            List<String> uris = new ArrayList<String>();
+            uris.add(appName + "." + targetName);
+            List<String> services = new ArrayList<String>();
+            client.createApplication(appName, staging, 512, uris, services);
+        }
+
+        System.out.println("\nUploading app " + appName);
+        File warFile = new File(warPath);
+        client.uploadApplication(appName, warFile);
     }
 
     private static URL getTargetURL(String target) {
